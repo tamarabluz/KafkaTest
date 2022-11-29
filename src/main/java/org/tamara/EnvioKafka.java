a9de6a7a09a5f02c2bb8b874b319c5c07096d9bf
tamara luz <tamarabluz@gmail.com>
2022-11-29 12:01:29 -0300

    sending

---
+++ src/main/java/org/tamara/EnvioKafka.java
@@ -0,0 +1,27 @@
+package org.tamara;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+
+public class EnvioKafka {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer <String, String> producer = new KafkaProducer(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("testJava", "Olá mundo");
+
+        producer.send(record);
+        producer.close();
+
+
+    }
+}
