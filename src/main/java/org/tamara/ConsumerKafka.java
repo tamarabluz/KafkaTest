57dc68cdd8f28ec96cf48a69d454f94f7e963d0f
tamara luz <tamarabluz@gmail.com>
2022-11-30 11:01:41 -0300

    New While

--- src/main/java/org/tamara/ConsumerKafka.java
+++ src/main/java/org/tamara/ConsumerKafka.java
@@ -21,22 +21,20 @@ public class ConsumerKafka {
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");//infoma para o kafka que quero as msg mais antigas;
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");//infoma para o kafka que quero as msg mais antigas;
 
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties)) {
 
-        consumer.subscribe(Arrays.asList("testJava"));
+            consumer.subscribe(Arrays.asList("testJava"));
 
-        //comando que vai aguardar pelas msg;
-        ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            //comando que vai aguardar pelas msg;
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
 
-        for (ConsumerRecord<String, String> record : records){
-            System.out.println(record.value());
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println(record.value());
+                }
+            }
         }
-        consumer.close();
-
-        System.out.println("Fim");
-
-
     }
 }
